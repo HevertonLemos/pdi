ee1c741f7a04e7b718bc5372e021bd58820aa189
hevertonlemos <hevertoncn@gmail.com>
2015-11-02 22:07:52 -0200

    correcao

--- src/Alerta.cpp
+++
@@ -1,9 +0,0 @@
-#include "include/Alerta.h"
-
-void alertMensagem(std::string frase)
-{
-    QMessageBox msg;
-    msg.setWindowTitle("Alerta");
-    msg.setText(QString::fromStdString(frase));
-    msg.exec();
-}
